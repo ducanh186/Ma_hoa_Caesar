5902452265762c35cb51efaa0db427e11e5c1b73
ducanh186 <leducanh180604@gmail.com>
2024-06-09 02:18:20 +0700

    Sieu cap final cho nam doc

--- main.cpp
+++ main.cpp
@@ -2,26 +2,17 @@
 #include <string.h>
 #include <iomanip>
 using namespace std;
-
-int GetMode(char* input_prompt, char* confirm_message) {
-    int option = 0;
-    do {
-        printf("\n%s: ", input_prompt);
-        scanf("%d", &option); // Read the selected mode from the user
-        while (getchar() != '\n'); // Clear the input buffer. Input buffer always end with Enter ('\n')
-        printf("\n%s: %d\n",confirm_message, option);
-     // Display the confirmed mode to the user
-    } while(!(option >= 1 && option <= 3)); // Repeat until a valid mode is selected
-    return  option;
-}
-string encrypt(string text, int key)
+string encrypt(string text, int key)// truyền vâo chuỗi cần giải mã và key
 {
-	string result = "";
-	for (int i = 0; i < (int)text.length(); i++) {
-		if (isupper(text[i]))
-			result += char(int(text[i] + key - 65) % 26 + 65);
-		else
-			result += char(int(text[i] + key - 97) % 26 + 97);
+	string result = "";// khai báo 1 chuỗi trống 
+	for (int i = 0; i < (int)text.length(); i++) {//xét từng phần tử của chuỗi
+		if (isupper(text[i]))//nếu viết hoa thì ...
+		result += char(int(text[i] + key - 65) % 26 + 65);//A =
+		//Chuyển số thứ tự từ ASCII sang 0-26 để thêm bớt keyint//(text[i] + key - 65) % 26//
+		//Sau đó, lại chuyển về mã ASCII để gắn vào biến result(+65)
+
+		else//nếu viết thường thì tương tự với viết hoa
+		result += char(int(text[i] + key - 97) % 26 + 97);
 	}
 	return result;
 }
@@ -39,26 +30,23 @@ string decrypt(string text, int key){
 }
 int main()
 {            while(1)  {
-	cout<<setw(11)<<"Cesar"<<endl;
-	cout<<setw(30)<<"P = C = K = Zn"<<endl;
-	cout<<setw(35)<<"ek(x) = (x+k) mod n"<<endl;
-	cout<<setw(35)<<"dk(y) = (y-k) mod n"<<endl;
-	cout<<setw(19)<<"Ex:"<<endl;
-	cout<<setw(37)<<"plaintext: ATTACK"<<endl;
-	cout<<setw(27)<<"key: 17"<<endl;
-	cout<<setw(38)<<"ciphertext: RKKRTB"<<endl;
-	cout<<endl;
-	cout<<"---MA HOA CAESAR---"<<endl;
+	cout<<"\n---MA HOA CAESAR---\n"<<endl;
                cout<<"  1.Giai Ma"<<endl;
                cout<<"  2.Ma Hoa"<<endl;
                cout<<"  3.Exit"<<endl;
-               //Menu
-	int option=GetMode("Select mode [1-3]", "Mode selected") ; 
+	int option = 0;
+	do {
+	printf("Nhap lua chon[1-3]: ");
+	scanf("%d", &option); // Read the selected mode from the user
+	while (getchar() != '\n'); // Clear the input buffer. Input buffer always end with Enter ('\n')
+	printf("\nLua chon hien tai la:%d\n", option);
+	// Display the confirmed mode to the user
+	} while(!(option >= 1 && option <= 3));
 	if(option==1){
-	 cout<<setw(23)<<"___GIAI MA___";
-	 cout<<setfill('-')<<setw(61)<<endl;
+	system("cls");
+	 cout<<setw(23)<<"\n___MA HOA___\n";
 	 string text;
-	 cout<<"\nNhap text can giai ma:";
+	 cout<<"\nNhap text can ma hoa:";
 	 cin.ignore();
 	 getline(cin,text);
 	 int key;
@@ -66,9 +54,10 @@ int main()
 	 cin>>key;
 	 cout<<"Cipher: "<<encrypt(text,key)<<endl;
 	}else if(option==2){
-	cout<<"___MA HOA___"<<endl;
+	system("cls");
+	cout<<"\n___GIAI MA___\n"<<endl;
 	 string text;
-	 cout<<"Nhap text can giai ma:";
+	 cout<<"\nNhap text can giai ma:";
 	 cin.ignore();
 	 getline(cin,text);
 	 int key;
