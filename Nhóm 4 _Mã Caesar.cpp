921c31bcc4b5f5b0a90cdec7926764b2f7177e83
ducanh186 <leducanh180604@gmail.com>
2024-06-11 20:28:26 +0700

    [ADD] split string in put on table

--- Nhóm 4 _Mã Caesar.cpp
+++ Nhóm 4 _Mã Caesar.cpp
@@ -31,9 +31,56 @@ string decrypt(string text, int key){//Giải mã cũng tương tự nhưng - ke
 	}
 	return result;
 }
+void printCaesarCipherTable() {
+	int shift =0;
+    cout<<setw(47)<<"Bang gia tri tuong ung"<<endl;
+  for (char c = 'a'; c <= 'z'; c++) {
+    char ciphertext = (c + shift - 'a') % 26 + 'a';
+       printf("|%-2c", c);
+  }
+  cout << endl;
+  for (char c = 'A'; c <= 'Z'; c++) {
+    char ciphertext = (c + shift - 'a') % 26 + 'a';
+       printf("|%-2c", c);
+  }
+  cout << endl;
+  for (int i = 0; i < 26; i++) {
+    printf("|%-2d", i);
+  }
+  cout << endl;
+}
+void In_bang_ma_hoa (string text,int key ){
+    string enc= encrypt(text,key);
+    cout<<"Key: "<<key<<endl;
+    for (int i = 0; i < (int)text.length(); i++){
+        if (islower(text[i])){
+            cout<< text[i]<<":("<<int(text[i] - 97) % 26<<")"<<"  -->  "<<enc[i]<<":("<<int(enc[i]-97)%26<<")"<<endl;
+            }
+        else {
+                        cout<< text[i]<<":("<<int(text[i] - 65) % 26<<")"<<"  -->  "<<enc[i]<<":("<<int(enc[i]-65)%26<<")"<<endl;
+        }
+    }
+     cout <<endl;
+
+}
+void In_bang_giai_ma (string text,int key ){
+    string enc= decrypt(text,key);
+    cout<<"Key: "<<key<<endl;
+    for (int i = 0; i < (int)text.length(); i++){
+        if (islower(text[i])){
+            cout<< text[i]<<":("<<int(text[i] - 97) % 26<<")"<<"  -->  "<<enc[i]<<":("<<int(enc[i]-97)%26<<")"<<endl;
+            }
+        else {
+                        cout<< text[i]<<":("<<int(text[i] - 65) % 26<<")"<<"  -->  "<<enc[i]<<":("<<int(enc[i]-65)%26<<")"<<endl;
+        }
+    }
+     cout <<endl;
+
+}
 int main()
 {            while(1)  {
 	cout<<"\n---MA HOA CAESAR---\n"<<endl;
+	printCaesarCipherTable();
 	cout<<"  1.Ma Hoa"<<endl;
                cout<<"  2.Giai Ma"<<endl;              
                cout<<"  3.Exit"<<endl;
@@ -45,26 +92,29 @@ int main()
 	printf("\nLua chon hien tai la:%d\n", option);
 	} while(!(option >= 1 && option <= 3));
 	if(option==1){
-	system("cls");
+	//system("cls");
 	 cout<<setw(23)<<"\n___MA HOA___\n";
 	 string text;
 	 cout<<"\nNhap text can ma hoa: ";
 	 getline(cin,text);
 	 int key;
-	 cout<<"Shift: ";
+	 cout<<"Shift[1-26]: ";
 	 cin>>key;
 	 cin.ignore();
-	 cout<<"Cipher: "<<encrypt(text,key)<<endl;
+	 string crypted_text = encrypt(text,key);
+	 In_bang_ma_hoa(text, key);
+	 cout<<"Cipher: "<<crypted_text<<endl;
 	}else if(option==2){
-	system("cls");
+	//system("cls");
 	cout<<"\n___GIAI MA___\n"<<endl;
 	 string text;
 	 cout<<"\nNhap text can giai ma: ";
 	 getline(cin,text);
 	 int key;
-	 cout<<"Shift: ";
+	 cout<<"Shift[1-26]: ";
 	 cin>>key;
 	 cin.ignore();
+	 In_bang_giai_ma(text, key);
 	 cout<<"Plain: "<<decrypt(text,key)<<endl;
 	}else if(option==3){
 		cout<<"\nExit program!"<<endl;
